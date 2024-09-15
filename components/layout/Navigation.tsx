86c7607ad9cba17356939d1a371953373e168a91
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-09-15 18:41:10 +0300

    Navigation for desktop complete

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -11,10 +11,19 @@ export default function Navigation() {
 			<a href="/">
 				<Logo />
 			</a>
+			<div className="hidden sm:flex">
+				<a href="#Anasayfa" className="text-primaryText hover:text-primary mx-3 text-2xl">Anasayfa</a>
+				<a href="#Mum" className="text-primaryText hover:text-primary mx-3 text-2xl">Mum</a>
+				<a href="#Yuzuk" className="text-primaryText hover:text-primary mx-3 text-2xl">Yüzük</a>
+				<a href="#Kolye" className="text-primaryText hover:text-primary mx-3 text-2xl">Kolye</a>
+				<a href="#Bijuteri" className="text-primaryText hover:text-primary mx-3 text-2xl">Bijuteri</a>
+				<a href="#Kupe" className="text-primaryText hover:text-primary mx-3 text-2xl">Küpe</a>
+				<a href="#Bileklik" className="text-primaryText hover:text-primary mx-3 text-2xl">Bileklik</a>
+				<a href="#Iletisim" className="text-primaryText hover:text-primary mx-3 text-2xl">İletişim</a>
+			</div >
 			<a href="#">
 				<CartIcon />
-
 			</a>
-		</nav>
+		</nav >
 	)
 }
